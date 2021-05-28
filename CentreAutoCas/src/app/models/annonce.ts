6f8171e4e7c84fc6a93025043d6139dfd22cd422
Ayoub Slimani <ayoub.slimanipro@gmail.com>
2021-05-28 16:16:00 +0200

    third commit

--- CentreAutoCas/src/app/models/annonce.ts
+++ CentreAutoCas/src/app/models/annonce.ts
@@ -1,3 +1,10 @@
 export class Annonce {
-  constructor(public id: string) {}
+  constructor(
+    public id: string,
+    public description: string,
+    public prix: number,
+    public reference: string,
+    public titre: string,
+    public dateAnnonce: string
+  ) {}
 }
